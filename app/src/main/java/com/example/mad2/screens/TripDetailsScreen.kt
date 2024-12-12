fcdada26104715df7757e3cbafa6f081192088b7
jonas <jonas.rysz@gmail.com>
2024-12-12 13:31:00 +0100

    profile screen done

--- app/src/main/java/com/example/mad2/screens/TripDetailsScreen.kt
+++ app/src/main/java/com/example/mad2/screens/TripDetailsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TripDetailsScreen(navController: NavHostController) {
